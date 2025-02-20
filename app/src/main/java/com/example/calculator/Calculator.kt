0c0074412bc2e49897d39ef92347c09d7929967f
svinec <alexandra.korzh@gmail.com>
2025-02-20 12:16:11 +0300

    feat MULTIPLY & DIVIDE

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -3,69 +3,84 @@ package com.example.calculator
 import android.util.Log
 import androidx.core.text.isDigitsOnly
 
+const val ADDITION = "+"
+const val SUBTRACT = "-"
+const val MULTIPLY = "x"   // todo написать все 4 коснатнты в коде
+const val DIVIDE = "÷"
+
+
 class Calculator() {
     private var result: Int = 0
-    private var num1: Int = 0
-    private var num2: Int = 0
+    private var num1: String = ""
+    private var num2: String = ""
+
     private var flag: Boolean = false // в  фолс - 1е число, в тру - 2е
     private var operation: String = ""
 
     fun getNumber(num: String) {
-        if (num == "+") {
+        if (num == ADDITION) {
             operation = num
             flag = true
-        } else if (num == "-") {
+        } else if (num == SUBTRACT) {
             operation = num
             flag = true
 
-        } else if (num == "*") {
+        } else if (num == MULTIPLY) {
+            operation = num
+            flag = true
+        } else if (num == DIVIDE) {
             operation = num
             flag = true
         }
 
         else if (num.isDigitsOnly()) {
-
             if (flag == false) {
-                num1 = Integer.parseInt(num)
+                num1 = num1 + num
             } else {
-                num2 = Integer.parseInt(num)
+                num2 = num2 + num
             }
-
-
         } else if (num == "=") {
-            val a = equal()
+            var num1Int = Integer.parseInt(num1)
+            var num2Int = Integer.parseInt(num2)
+            val a = equal(num1Int, num2Int)
+
             flag = false
-            Log.e("eee", a.toString())    // логирование
+            Log.e("eee", "num1 = $num1")
+            Log.e("eee", "num2 = $num2")
+            Log.e("eee", a.toString()) // логирование
+            result = 0
+            num1 = ""
+            num2 = ""
         }
 
     }
 
 
-    fun add() {
-        result = num1 + num2
+    fun add(num1Int: Int, num2Int: Int) {
+        result = num1Int + num2Int
     }
 
-    fun subract() {
-        result = num1 - num2
+    fun subtract(num1Int: Int, num2Int: Int) {
+        result = num1Int - num2Int
     }
 
-    fun multiply() {
-        result = num1 * num2
+    fun multiply(num1Int: Int, num2Int: Int) {
+        result = num1Int * num2Int
     }
 
-    fun devide() {
-        result = num1 / num2
+    fun devide(num1Int: Int, num2Int: Int) {
+        result = num1Int / num2Int
     }
 
-    fun equal(): Int {
-        if (operation == "+") {
-            add()
-        } else if (operation == "-") {
-            subract()
-        } else if (operation == "*") {
-            multiply()
-        } else if (operation == "/"){
-            devide()
+    fun equal(num1Int: Int, num2Int: Int): Int {
+        if (operation == ADDITION) {
+            add(num1Int, num2Int)
+        } else if (operation == SUBTRACT) {
+            subtract(num1Int, num2Int)
+        } else if (operation == MULTIPLY) {
+            multiply(num1Int, num2Int)
+        } else if (operation == DIVIDE) {
+            devide(num1Int, num2Int)
         }
         return result
     }
